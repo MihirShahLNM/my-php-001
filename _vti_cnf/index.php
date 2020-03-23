56f4a38db4504424a4049068cebcb6a796b658d5
Mihir Shah <mihir.shah@light-and-magic.com>
2020-03-23 16:30:28 +0530

    a normal change

---
+++ _vti_cnf/index.php
@@ -0,0 +1,17 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|23 Mar 2020 09:10:24 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DELL-PROG\\Mihir
+vti_modifiedby:SR|DELL-PROG\\Mihir
+vti_timecreated:TR|23 Mar 2020 07:08:53 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|23 Mar 2020 09:09:53 -0000
+vti_cacheddtm:TX|23 Mar 2020 09:10:24 -0000
+vti_filesize:IR|182
+vti_cachedlinkinfo:VX|I|header.php I|sidebar.php I|footer.php
+vti_cachedsvcrellinks:VX|FIUS|my-php-001/header.php FIUS|my-php-001/sidebar.php FIUS|my-php-001/footer.php
+vti_cachedneedsrewrite:BR|true
+vti_cachedhasbots:BR|true
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
